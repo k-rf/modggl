132c741289427f251edfc7d5036448008a1179f9
Kawano Kenichi <kenichi.k.820@gmail.com>
2022-01-18 13:48:33 +0000

    :art: Update interactor
    Slack に通知する用の Presenter を処理中に組み込む。
    通知する処理はまだ実装していない。

--- modggl/src/features/modification/application/usecase/modify_entry_interactor.rs
+++ modggl/src/features/modification/application/usecase/modify_entry_interactor.rs
@@ -4,12 +4,14 @@ use async_trait::async_trait;
 
 use super::domain::entry::{EntryList, EntrySince, EntryUntil};
 use super::domain::{ActionType, EntryLogger, EntryReviser, ResultModified, ReviserStatus};
-use super::port::incoming::ModifyEntryCommand;
-use super::port::incoming::ModifyEntryUsecase;
-use super::port::outgoing::EntryTogglRepositoryPort;
+use super::port::incoming::{ModifyEntryCommand, ModifyEntryUsecase};
+use super::port::outgoing::{
+    EntryModifiedOutputData, EntryModifiedPresenterPort, EntryRepositoryPort,
+};
 
 pub struct ModifyEntryInteractor {
-    pub toggl_repository_port: Arc<dyn EntryTogglRepositoryPort + Sync + Send>,
+    pub entry_repository_port: Arc<dyn EntryRepositoryPort + Sync + Send>,
+    pub entry_modified_presenter_port: Arc<dyn EntryModifiedPresenterPort + Sync + Send>,
 }
 
 #[async_trait]
@@ -17,7 +19,7 @@ impl ModifyEntryUsecase for ModifyEntryInteractor {
     async fn execute(&self, command: ModifyEntryCommand) {
         let since = EntrySince::new(command.since);
         let until = EntryUntil::new(command.until);
-        let entries = self.toggl_repository_port.get(since, until).await;
+        let entries = self.entry_repository_port.get(since, until).await;
 
         let mut modification_list = EntryList::new(vec![]);
         let mut deletion_list = EntryList::new(vec![]);
@@ -26,27 +28,44 @@ impl ModifyEntryUsecase for ModifyEntryInteractor {
 
         for entry in entries.value.into_iter() {
             if let Ok(e) = reviser.insert(entry) {
-                match e {
-                    ReviserStatus::Full => {
-                        if let Ok(e) = reviser.merge() {
-                            modification_list.upsert(e.merged);
-                            deletion_list.upsert(e.deletable);
-                            continue;
-                        }
-
-                        match reviser.modify() {
-                            ResultModified::Modified(modified) => {
-                                modification_list.upsert(modified)
-                            }
-                            // TODO: Presenter を実装し、外部に通知できるようにする
-                            // 修正中にエラーパターンが出てきたら、ここで Presenter を呼び出す
-                            _ => (),
-                        }
-                    }
-                    _ => {
+                if let ReviserStatus::Full = e {
+                    if let Ok(e) = reviser.merge() {
+                        modification_list.upsert(e.merged);
+                        deletion_list.upsert(e.deletable);
                         continue;
                     }
-                }
+
+                    match reviser.modify() {
+                        ResultModified::Modified(modified) => {
+                            modification_list.upsert(modified);
+                        }
+                        ResultModified::Unnecessary => {}
+                        ResultModified::InvalidOrder => {
+                            let message =
+                                String::from("The retrieved values are in an invalid order.");
+
+                            log::error!("{}", message);
+                            self.entry_modified_presenter_port
+                                .execute(EntryModifiedOutputData { message })
+                                .await;
+
+                            return;
+                        }
+                        ResultModified::NotDetermine => {
+                            let message =
+                                String::from("Cannot modify an entry. Please modify it manually.");
+
+                            log::error!("{}", message);
+                            self.entry_modified_presenter_port
+                                .execute(EntryModifiedOutputData { message })
+                                .await;
+
+                            return;
+                        }
+                    }
+                } else {
+                    continue;
+                };
             }
         }
 
@@ -54,17 +73,19 @@ impl ModifyEntryUsecase for ModifyEntryInteractor {
 
         for entry in modification_list.value.into_iter() {
             info_logger(&entry, ActionType::Modify);
-            self.toggl_repository_port.modify(entry).await;
+            self.entry_repository_port.modify(entry).await;
         }
 
         for entry in deletion_list.value.into_iter() {
             info_logger(&entry, ActionType::Delete);
-            self.toggl_repository_port.delete(entry).await;
+            self.entry_repository_port.delete(entry).await;
         }
 
-        // TODO: Presenter を実装し、外部に通知できるようにする
-        // 成功した場合はここで通知するようにする
-        log::info!("Modification is completed.");
+        let message = String::from("Modification is completed.");
+        log::info!("{}", message);
+        self.entry_modified_presenter_port
+            .execute(EntryModifiedOutputData { message })
+            .await;
     }
 }
 
@@ -73,6 +94,7 @@ mod tests {
     use tokio;
 
     use crate::features::modification::application::port::incoming::ModifyEntryCommand;
+    use crate::features::modification::presenter::EntryModifiedSlackPresenter;
     use crate::features::modification::repository::EntryTogglRepository;
     use crate::services::LoggerService;
     use crate::utils;
@@ -85,7 +107,8 @@ mod tests {
         dotenv::from_filename(".env.local").ok();
 
         let interactor = ModifyEntryInteractor {
-            toggl_repository_port: Arc::new(EntryTogglRepository::new()),
+            entry_repository_port: Arc::new(EntryTogglRepository::new()),
+            entry_modified_presenter_port: Arc::new(EntryModifiedSlackPresenter::new()),
         };
 
         interactor
--- modggl/src/routes/config.rs
+++ modggl/src/routes/config.rs
@@ -4,11 +4,13 @@ use actix_web::{web, Responder};
 
 use crate::features::modification::application::usecase::ModifyEntryInteractor;
 use crate::features::modification::controller::ModifyEntryController;
+use crate::features::modification::presenter::EntryModifiedSlackPresenter;
 use crate::features::modification::repository::EntryTogglRepository;
 
 async fn modify() -> impl Responder {
     let usecase = ModifyEntryInteractor {
-        toggl_repository_port: Arc::new(EntryTogglRepository::new()),
+        entry_repository_port: Arc::new(EntryTogglRepository::new()),
+        entry_modified_presenter_port: Arc::new(EntryModifiedSlackPresenter::new()),
     };
     let controller = ModifyEntryController {
         usecase: Arc::new(usecase),
